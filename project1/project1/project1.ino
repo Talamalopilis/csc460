438fde080e7005981cff4026c2b46e7ff81d8b16
Tal Melamed <tal.melamed.69@gmail.com>
2018-01-29 14:02:23 -0800

    init project

---
+++ project1/project1/project1.ino
@@ -0,0 +1,15 @@
+/*
+ Name:		project1.ino
+ Created:	1/29/2018 2:01:00 PM
+ Author:    group3
+*/
+
+// the setup function runs once when you press reset or power the board
+void setup() {
+
+}
+
+// the loop function runs over and over again until power down or reset
+void loop() {
+  
+}
