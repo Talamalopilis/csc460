9ef7d177761946193dd2d4f72271a9fc5e65c2e1
tyfe <fisher.tyler2@gmail.com>
2018-01-30 12:47:58 -0800

    light sensor

--- project1/project1/project1.ino
+++ project1/project1/project1.ino
@@ -2,6 +2,8 @@
 
 int JoyStick_X = A0;
 int JoyStick_Y = A1;
+int LIGHT_SENSOR = A2;
+
 int JoyStick_Z = 32;
 int LED_PIN = 13;
 const int panpin = 9;
@@ -12,70 +14,77 @@ int tpos = 1500;
 Servo pan;
 Servo tilt;
 
-
-void setup() {
-	// put your setup code here, to run once:
-	pinMode(JoyStick_X, INPUT);
-	pinMode(JoyStick_Y, INPUT);
-	pinMode(JoyStick_Z, INPUT_PULLUP);
-	pinMode(13, OUTPUT);
-	pinMode(38, OUTPUT);
-	Serial.begin(9600);
-	pan.attach(panpin);
-	tilt.attach(tiltpin);
-	pan.writeMicroseconds(ppos);
-	tilt.writeMicroseconds(tpos);
-	delay(200);
-	
-
+void setup()
+{
+  // put your setup code here, to run once:
+  pinMode(JoyStick_X, INPUT);
+  pinMode(JoyStick_Y, INPUT);
+  pinMode(JoyStick_Z, INPUT_PULLUP);
+  pinMode(13, OUTPUT);
+  pinMode(38, OUTPUT);
+  Serial.begin(9600);
+  pan.attach(panpin);
+  tilt.attach(tiltpin);
+  pan.writeMicroseconds(ppos);
+  tilt.writeMicroseconds(tpos);
+  delay(200);
 }
 
-void loop() {
-	float x, y;
-	int z;
-	x = analogRead(JoyStick_X);
-	y = analogRead(JoyStick_Y);
-	while (1) {
-		x = alpha * analogRead(JoyStick_X) + alpha * x;
-		y = alpha * analogRead(JoyStick_Y) + alpha * y;
-		z = digitalRead(JoyStick_Z);
+void loop()
+{
+  float x, y;
+  int z, ls0;
+  x = analogRead(JoyStick_X);
+  y = analogRead(JoyStick_Y);
+  while (1)
+  {
+    x = alpha * analogRead(JoyStick_X) + alpha * x;
+    y = alpha * analogRead(JoyStick_Y) + alpha * y;
+    z = digitalRead(JoyStick_Z);
+    ls0 = analogRead(LIGHT_SENSOR);
 
-		// just use x axis to adjust brightness
-		ppos +=  ((int)x - 509) / 50;
-		tpos += ((int)y - 510) / 50;
+    // just use x axis to adjust brightness
+    ppos += ((int)x - 509) / 50;
+    tpos += ((int)y - 510) / 50;
 
-		if (ppos > 2000) {
-			ppos = 2000;
-		}
-		else if (ppos < 1000) {
-			ppos = 1000;
-		}
-		if (tpos > 2000) {
-			tpos = 2000;
-		}
-		else if (tpos < 1000) {
-			tpos = 1000;
-		}
-		if (!z) {
-			digitalWrite(38, HIGH);
-		}
-		else {
-			digitalWrite(38, LOW);
-		}
-		pan.writeMicroseconds(ppos);
-		tilt.writeMicroseconds(tpos);
-		analogWrite(13, map(ppos, 1000, 2000, 0, 255));                           
-		    Serial.print ((int)x, DEC);
-		    Serial.print (",");
-		    Serial.print ((int)y, DEC);
-		    Serial.print (",");
-			Serial.print(z);
-			Serial.print(",");
-		    Serial.println ((int)ppos, DEC);
+    if (ppos > 2000)
+    {
+      ppos = 2000;
+    }
+    else if (ppos < 1000)
+    {
+      ppos = 1000;
+    }
+    if (tpos > 2000)
+    {
+      tpos = 2000;
+    }
+    else if (tpos < 1000)
+    {
+      tpos = 1000;
+    }
+    if (!z)
+    {
+      digitalWrite(38, HIGH);
+    }
+    else
+    {
+      digitalWrite(38, LOW);
+    }
+    pan.writeMicroseconds(ppos);
+    tilt.writeMicroseconds(tpos);
+    analogWrite(LED_PIN, 0);
+    Serial.print((int)x, DEC);
+    Serial.print(",");
+    Serial.print((int)y, DEC);
+    Serial.print(",");
+    Serial.print(z);
+    Serial.print(",");
+    Serial.print((int)ppos, DEC);
 
-			Serial.print(",");
-			Serial.println((int)tpos, DEC);
-		delay(10);
-	}
-	//  
+    Serial.print(",");
+    Serial.println(ls0, DEC);
+    delay(10);
+  }
+  //
 }
