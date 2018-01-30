ca39aaa77b55aae481cd8465a135853c1db48cd8
Tal Melamed <tal.melamed.69@gmail.com>
2018-01-29 16:45:25 -0800

    motor control with joystick, laser click

--- project1/project1/project1.ino
+++ project1/project1/project1.ino
@@ -1,15 +1,81 @@
-/*
- Name:		project1.ino
- Created:	1/29/2018 2:01:00 PM
- Author:    group3
-*/
+#include <Servo.h>
 
-// the setup function runs once when you press reset or power the board
+int JoyStick_X = A0;
+int JoyStick_Y = A1;
+int JoyStick_Z = 32;
+int LED_PIN = 13;
+const int panpin = 9;
+int const tiltpin = 10;
+float alpha = 0.5;
+int ppos = 1500;
+int tpos = 1500;
+Servo pan;
+Servo tilt;
+
+
 void setup() {
+	// put your setup code here, to run once:
+	pinMode(JoyStick_X, INPUT);
+	pinMode(JoyStick_Y, INPUT);
+	pinMode(JoyStick_Z, INPUT_PULLUP);
+	pinMode(13, OUTPUT);
+	pinMode(38, OUTPUT);
+	Serial.begin(9600);
+	pan.attach(panpin);
+	tilt.attach(tiltpin);
+	pan.writeMicroseconds(ppos);
+	tilt.writeMicroseconds(tpos);
+	delay(200);
+	
 
 }
 
-// the loop function runs over and over again until power down or reset
 void loop() {
-  
+	float x, y;
+	int z;
+	x = analogRead(JoyStick_X);
+	y = analogRead(JoyStick_Y);
+	while (1) {
+		x = alpha * analogRead(JoyStick_X) + alpha * x;
+		y = alpha * analogRead(JoyStick_Y) + alpha * y;
+		z = digitalRead(JoyStick_Z);
+
+		// just use x axis to adjust brightness
+		ppos +=  ((int)x - 509) / 50;
+		tpos += ((int)y - 510) / 50;
+
+		if (ppos > 2000) {
+			ppos = 2000;
+		}
+		else if (ppos < 1000) {
+			ppos = 1000;
+		}
+		if (tpos > 2000) {
+			tpos = 2000;
+		}
+		else if (tpos < 1000) {
+			tpos = 1000;
+		}
+		if (!z) {
+			digitalWrite(38, HIGH);
+		}
+		else {
+			digitalWrite(38, LOW);
+		}
+		pan.writeMicroseconds(ppos);
+		tilt.writeMicroseconds(tpos);
+		analogWrite(13, map(ppos, 1000, 2000, 0, 255));                           
+		    Serial.print ((int)x, DEC);
+		    Serial.print (",");
+		    Serial.print ((int)y, DEC);
+		    Serial.print (",");
+			Serial.print(z);
+			Serial.print(",");
+		    Serial.println ((int)ppos, DEC);
+
+			Serial.print(",");
+			Serial.println((int)tpos, DEC);
+		delay(10);
+	}
+	//  
 }
