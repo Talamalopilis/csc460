cec8121d20fd99aef11c8724f0cf5bea8d113ccc
Tal Melamed <tal.melamed.69@gmail.com>
2018-02-01 16:06:19 -0800

    refactor into functions

--- project1/project1lcd.ino
+++ project1/project1lcd.ino
@@ -4,98 +4,135 @@
 int JoyStick_X = A1;
 int JoyStick_Y = A2;
 int LIGHT_SENSOR = A3;
-
 int JoyStick_Z = 32;
 int LED_PIN = 13;
+int laserpin = 38;
 const int panpin = 2;
 int const tiltpin = 3;
 float alpha = 0.5;
-int ppos = 1500;
-int tpos = 1500;
 
+struct joyvals {
+	float x, y;
+	int z;
+};
+
+struct controlstate {
+	int ppos = 1500;
+	int tpos = 1500;
+	bool laser = false;
+	int ls = 0;
+};
+
+struct controlstate cs;
+struct joyvals j;
 Servo pan;
 Servo tilt;
 LiquidCrystal lcd(8, 9, 4, 5, 6, 7);
+void getjoystick(struct joyvals*, struct controlstate*);
+void getlightsensor(struct controlstate*);
+void changestate(struct controlstate*);
+void printlcd(struct joyvals*, struct controlstate*);
+
+void getjoystick(struct joyvals *j, struct controlstate *cs) {
+	j->x = analogRead(JoyStick_X);
+	j->y = analogRead(JoyStick_Y);
+	j->z = digitalRead(JoyStick_Z);
+	int ppos = cs->ppos;
+	int tpos = cs->tpos;
+
+	ppos += ((int)j->x - 509) / 50;
+	tpos += ((int)j->y - 510) / 50;
+
+	if (ppos > 2000)
+	{
+		ppos = 2000;
+	}
+	else if (ppos < 1000)
+	{
+		ppos = 1000;
+	}
+	if (tpos > 2000)
+	{
+		tpos = 2000;
+	}
+	else if (tpos < 1000)
+	{
+		tpos = 1000;
+	}
+	if (!j->z) {
+		cs->laser = true;
+	}
+	else {
+		cs->laser = false;
+	}
+	cs->ppos = ppos;
+	cs->tpos = tpos;
+}
+
+void getlightsensor(controlstate *cs)
+{
+	cs->ls = analogRead(LIGHT_SENSOR);
+}
+
+void changestate(controlstate *cs)
+{
+	pan.writeMicroseconds(3000 - cs->ppos);
+	tilt.writeMicroseconds(cs->tpos);
+	digitalWrite(laserpin, cs->laser);
+}
+
+void printlcd(joyvals *j, controlstate *cs)
+{
+	lcd.home();
+	lcd.print(3000 - cs->ppos);
+	lcd.print(" ");
+	lcd.print(cs->tpos);
+	lcd.setCursor(0, 1);
+	lcd.print("LSR ");
+	lcd.print(cs->laser ? " ON " : "OFF ");
+	lcd.print(cs->ls >= 1000 ? 999 : cs->ls);
+
+	Serial.print((int)j->x, DEC);
+	Serial.print(",");
+	Serial.print((int)j->y, DEC);
+	Serial.print(",");
+	Serial.print(j->z);
+	Serial.print(",");
+	Serial.print((int)cs->ppos, DEC);
+
+	Serial.print(",");
+	Serial.println(cs->ls, DEC);
+	delay(10);
+}
+
+
 void setup()
 {
-  // put your setup code here, to run once:
-  pinMode(JoyStick_X, INPUT);
-  pinMode(JoyStick_Y, INPUT);
-  pinMode(JoyStick_Z, INPUT_PULLUP);
-  pinMode(13, OUTPUT);
-  pinMode(38, OUTPUT);
-  Serial.begin(9600);
-  lcd.begin(16, 2);
-  lcd.setCursor(0,0);
-  pan.attach(panpin);
-  tilt.attach(tiltpin);
-  pan.writeMicroseconds(ppos);
-  tilt.writeMicroseconds(tpos);
-  delay(200);
+	// put your setup code here, to run once:
+	pinMode(JoyStick_X, INPUT);
+	pinMode(JoyStick_Y, INPUT);
+	pinMode(JoyStick_Z, INPUT_PULLUP);
+	pinMode(13, OUTPUT);
+	pinMode(38, OUTPUT);
+	Serial.begin(9600);
+	lcd.begin(16, 2);
+	lcd.setCursor(0, 0);
+	pan.attach(panpin);
+	tilt.attach(tiltpin);
+	pan.writeMicroseconds(cs.ppos);
+	tilt.writeMicroseconds(cs.tpos);
+	delay(200);
+
+	j.x = analogRead(JoyStick_X);
+	j.y = analogRead(JoyStick_Y);
+	j.z = 0;
 }
 
 void loop()
 {
-  float x, y;
-  int z, ls0;
-  x = analogRead(JoyStick_X);
-  y = analogRead(JoyStick_Y);
-  while (1)
-  {
-    x = alpha * analogRead(JoyStick_X) + alpha * x;
-    y = alpha * analogRead(JoyStick_Y) + alpha * y;
-    z = digitalRead(JoyStick_Z);
-    ls0 = analogRead(LIGHT_SENSOR);
-
-    ppos += ((int)x - 509) / 50;
-    tpos += ((int)y - 510) / 50;
-
-    if (ppos > 2000)
-    {
-      ppos = 2000;
-    }
-    else if (ppos < 1000)
-    {
-      ppos = 1000;
-    }
-    if (tpos > 2000)
-    {
-      tpos = 2000;
-    }
-    else if (tpos < 1000)
-    {
-      tpos = 1000;
-    }
-    if (!z)
-    {
-      digitalWrite(38, HIGH);
-    }
-    else
-    {
-      digitalWrite(38, LOW);
-    }
-    pan.writeMicroseconds(3000 - ppos);
-    tilt.writeMicroseconds(tpos);
-//    analogWrite(LED_PIN, 0);
-    lcd.home();
-    lcd.print(3000 - ppos);
-    lcd.print(" ");
-    lcd.print(tpos);
-    lcd.setCursor(0,1);
-    lcd.print("LSR ");
-    lcd.print(!z ? " ON " : "OFF ");
-    lcd.print(ls0 >= 1000 ? 999 : ls0);
-    
-    Serial.print((int)x, DEC);
-    Serial.print(",");
-    Serial.print((int)y, DEC);
-    Serial.print(",");
-    Serial.print(z);
-    Serial.print(",");
-    Serial.print((int)ppos, DEC);
-
-    Serial.print(",");
-    Serial.println(ls0, DEC);
-    delay(10);
-  }
+	getjoystick(&j, &cs);
+	getlightsensor(&cs);
+	changestate(&cs);
+	//  analogWrite(LED_PIN, 0);
+	printlcd(&j, &cs);
 }
