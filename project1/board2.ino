8302f746d0928b1efce69ba57336f99a486f91c6
tyfe <fisher.tyler2@gmail.com>
2018-02-07 17:41:42 -0800

    bluetooth write

--- project1/board2.ino
+++ project1/board2.ino
@@ -10,20 +10,26 @@ const int LED_PIN = 13;
 const float alpha = 0.5;
 const int idle_pin = 40;
 
+int ls = 0;
+
+struct controlstate {
+  uint16_t ppos;
+  uint16_t tpos;
+  bool laser;
+};
+
+union Data {
+  controlstate cs;
+  byte bt[sizeof(controlstate)];
+};
+
 struct joyvals {
   float x, y;
   int z;
 };
 
-struct controlstate {
-  int ppos = 1500;
-  int tpos = 1500;
-  bool laser = false;
-  int ls = 0;
-};
-
-struct controlstate cs;
 struct joyvals j;
+union Data data;
 LiquidCrystal lcd(8, 9, 4, 5, 6, 7);
 void getjoystick();
 void getlightsensor();
@@ -33,11 +39,11 @@ void getjoystick() {
   j.x = analogRead(JoyStick_X);
   j.y = analogRead(JoyStick_Y);
   j.z = digitalRead(JoyStick_Z);
-  int ppos = cs.ppos;
-  int tpos = cs.tpos;
+  int ppos = data.cs.ppos;
+  int tpos = data.cs.tpos;
 
-  ppos += ((int)j.x - 509) / 100;
-  tpos += ((int)j.y - 510) / 100;
+  ppos += ((int)j.x - 509) / 75;
+  tpos += ((int)j.y - 510) / 75;
 
   if (ppos > 2000)
   {
@@ -56,32 +62,36 @@ void getjoystick() {
     tpos = 1000;
   }
   if (!j.z) {
-    cs.laser = true;
+    data.cs.laser = true;
   }
   else {
-    cs.laser = false;
+    data.cs.laser = false;
   }
-  cs.ppos = ppos;
-  cs.tpos = tpos;
+  data.cs.ppos = ppos;
+  data.cs.tpos = tpos;
 }
 
 void getlightsensor()
 {
-  cs.ls = analogRead(LIGHT_SENSOR);
+  ls = analogRead(LIGHT_SENSOR);
 }
 
 void printlcd()
 {
   lcd.home();
-  lcd.print(3000 - cs.ppos);
+  lcd.print(3000 - data.cs.ppos);
   lcd.print(" ");
-  lcd.print(cs.tpos);
+  lcd.print(data.cs.tpos);
   lcd.setCursor(0, 1);
  // lcd.print("LSR ");
-  lcd.print(cs.laser ? "1 " : "0 ");
-  lcd.print(cs.ls >= 1000 ? 999 : cs.ls);
+  lcd.print(data.cs.laser ? "1 " : "0 ");
+  lcd.print(ls >= 1000 ? 999 : ls);
 }
 
+void writebt() {
+  Serial1.print('$');
+  Serial1.write(data.bt, sizeof(controlstate));
+}
 
 void setup()
 {
@@ -93,9 +103,12 @@ void setup()
   pinMode(13, OUTPUT);
   pinMode(38, OUTPUT);
   pinMode(idle_pin, OUTPUT);
-  //Serial.begin(9600);
+  Serial1.begin(9600);
   lcd.begin(16, 2);
   lcd.setCursor(0, 0);
+
+  data.cs.ppos = 1500;
+  data.cs.tpos = 1500;
 
   j.x = analogRead(JoyStick_X);
   j.y = analogRead(JoyStick_Y);
@@ -106,6 +119,7 @@ void setup()
   Scheduler_Init();
   Scheduler_StartTask(0, 10, getjoystick);
   Scheduler_StartTask(2, 40, getlightsensor);
+  Scheduler_StartTask(4, 30, writebt);
   Scheduler_StartTask(6, 500, printlcd);
 }
 
