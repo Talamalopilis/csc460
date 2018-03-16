760f06c310d7018a7af13b09b44cb81c21b663cc
tyfe <fisher.tyler2@gmail.com>
2018-03-15 19:11:16 -0700

    updated results array addressing to be slightly more sane

--- project2/project2/test_tasks.c
+++ project2/project2/test_tasks.c
@@ -12,13 +12,13 @@
 #define BUFFER_SIZE 1024
 
 unsigned char results[BUFFER_SIZE]; // use 1 kb of space for test results
-unsigned char * cur; // pointer to current character index in buffer
+uint16_t cur; // index of current character in buffer
 
 void basic_RR_test();
 void write_out();
 
 void test_main() {
-	cur = results;
+	cur = 0;
 	memset(results, 0, BUFFER_SIZE);
 	Task_Create_RR(basic_RR_test, 0);
 }
@@ -26,16 +26,16 @@ void test_main() {
 void basic_RR_test() {
 	int i;
 	for(i = 'a'; i < 'a' + 10; ++i) {
-		*cur++ = i % 0xff;
+		results[cur++] = i;
 	}
 	Task_Create_System(write_out, 0);
 }
 
 void write_out() {
-	unsigned char * p;
+	uint16_t p;
 	uart_init(BAUD_CALC(115200));
-	for(p = results; p < cur; ++p) {
-		uart0_putc(*p);
+	for(p = 0; p < cur; ++p) {
+		uart0_putc(results[p]);
 	}
 	uart0_putc('\r');
 	uart0_putc('\n');
